8de9c4de012ef59964c8fff993617ff916e36b1d
PaikBean <paikbean@gmail.com>
2024-11-23 22:12:54 +0900

    init

---
+++ src/main/java/com/fconlinelogger/FconlineLoggerManApplication.java
@@ -0,0 +1,13 @@
+package com.fconlinelogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FconlineLoggerManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FconlineLoggerManApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fconlinelogger/FconlineLoggerManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fconlinelogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FconlineLoggerManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
