010ec15a16df65bea95767e1607bf8956b1e3338
PaikBean <paikbean@gmail.com>
2024-11-24 15:52:07 +0900

    [FIX] Lombok getter 어노테이션 적용

--- src/main/java/com/fconlinelogger/exception/NexonApiException.java
+++ src/main/java/com/fconlinelogger/exception/NexonApiException.java
@@ -1,14 +1,13 @@
 package com.fconlinelogger.exception;
 
+import lombok.Getter;
+
+@Getter
 public class NexonApiException extends RuntimeException {
     private final String errorName;
 
     public NexonApiException(String errorName, String message) {
         super(message);
         this.errorName = errorName;
-    }
-
-    public String getErrorName() {
-        return errorName;
     }
 }
